de22e23f1725c0752f20dea9c738d322219ecbd2
LiminalCrab <devpreston@protonmail.com>
2021-03-23 08:00:01 -0400

    20210323_0800262600510

--- bulletproof.sh
+++ bulletproof.sh
@@ -1,8 +1,8 @@
 #!/bin/sh 
-export PGHOSTADDR=""
-export PGUSER=""
-export PGDATABASE=""
-export PGPASSWORD=""
+export PGHOSTADDR="172.17.0.1"
+export PGUSER="postgres"
+export PGDATABASE="rssfeeds"
+export PGPASSWORD="admin11"
 export PGPORT=5432
 
    
@@ -12,14 +12,11 @@ hostname=`ringzero`
   filename="/home/liminalcrab/Projects/fucking-bulletproof/${hostname}_${db}_${date}.sql"
   pg_dump rssfeeds >  $filename 2>&1
 
-
 cd /home/liminalcrab/Projects/fucking-bulletproof
 
 git add -A
 git commit -p -m "${date}"
 git push
 
+
 exit 0
-
-
-
