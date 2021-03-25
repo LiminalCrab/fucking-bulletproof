e95dffd8d36581b191fa5c801d67c8e15cccbf13
LiminalCrab <devpreston@protonmail.com>
2021-03-25 07:28:39 -0400

    helping hand.

--- __20210323_0443044791007.sql
+++
@@ -1,3 +0,0 @@
-pg_dump: error: connection to database "rssfeeds" failed: could not connect to server: No such file or directory
-	Is the server running locally and accepting
-	connections on Unix domain socket "/run/postgresql/.s.PGSQL.5432"?
